5ed26c684dba0ee0418115498a300fdd5dd75aff
patelsaajan <saajanpatel@hotmail.com>
2025-10-31 20:57:36 +0000

    feat: creating db migration files

--- src/migrations/index.ts
+++ src/migrations/index.ts
@@ -1,9 +1,15 @@
 import * as migration_20251031_162941 from './20251031_162941';
+import * as migration_20251031_205719 from './20251031_205719';
 
 export const migrations = [
   {
     up: migration_20251031_162941.up,
     down: migration_20251031_162941.down,
-    name: '20251031_162941'
+    name: '20251031_162941',
+  },
+  {
+    up: migration_20251031_205719.up,
+    down: migration_20251031_205719.down,
+    name: '20251031_205719'
   },
 ];
