9b89f901acf401ccae231edc91e2c68e4ce51139
patelsaajan <saajanpatel@hotmail.com>
2025-12-06 23:23:39 +0000

    feat: updating importmap using s3 package 3.26

--- src/app/(payload)/portal/importMap.js
+++ src/app/(payload)/portal/importMap.js
@@ -34,6 +34,7 @@ import { RowLabel as RowLabel_ec255a65fa6fa8d1faeb09cf35284224 } from '@/Header/
 import { ResetDefaults as ResetDefaults_5631ff4c228dd9fb59bf05a4b4037c08 } from '@/ThemeSettings/components/ResetDefaults'
 import { ColorPreview as ColorPreview_5720d74e97e8fcf58390365a656d25ae } from '@/ThemeSettings/components/ColorPreview'
 import { ButtonPreview as ButtonPreview_064f9d0db045c0324aefa2e11a2cbcf0 } from '@/ThemeSettings/components/ButtonPreview'
+import { S3ClientUploadHandler as S3ClientUploadHandler_f97aa6c64367fa259c5bc0567239ef24 } from '@payloadcms/storage-s3/client'
 
 export const importMap = {
   "@/heros/components/ColorPreview#ColorPreview": ColorPreview_bcb33648c10c8e7d0a493178413d3df3,
@@ -71,5 +72,6 @@ export const importMap = {
   "@/Header/RowLabel#RowLabel": RowLabel_ec255a65fa6fa8d1faeb09cf35284224,
   "@/ThemeSettings/components/ResetDefaults#ResetDefaults": ResetDefaults_5631ff4c228dd9fb59bf05a4b4037c08,
   "@/ThemeSettings/components/ColorPreview#ColorPreview": ColorPreview_5720d74e97e8fcf58390365a656d25ae,
-  "@/ThemeSettings/components/ButtonPreview#ButtonPreview": ButtonPreview_064f9d0db045c0324aefa2e11a2cbcf0
+  "@/ThemeSettings/components/ButtonPreview#ButtonPreview": ButtonPreview_064f9d0db045c0324aefa2e11a2cbcf0,
+  "@payloadcms/storage-s3/client#S3ClientUploadHandler": S3ClientUploadHandler_f97aa6c64367fa259c5bc0567239ef24
 }
